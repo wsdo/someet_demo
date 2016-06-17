f3216425cd535db978a38147cdc8154d7e48ed59
wshudong <wsd312@163.com>
2016-06-17 16:30:04 +0800

    初次版本

--- app/static/js/all.js
+++ app/static/js/all.js
@@ -1,58 +1,129 @@
 Zepto(function($) {
-// 加载flag
-      var loading = false;
-      // 最多可加载的条目
-      var maxItems = 100;
+    var page = 1;
+    var perPage = 10;
+    var loading = false;
+    var zWin = $(window);
+    var poster = {
+        "with": 800,
+        "height": 455,
+    }
+    var winWidth = zWin.width();
+    var winHeight = zWin.height();
+    var posterHeight = (winWidth / poster.with) * poster.height + 'px';
+    // var posterHeight = '100%';
+    var maxItems = 1000;
+    var getlist = function() {
+        $.ajax({
+            dataType: "json",
+            type: 'GET',
+            url: "./static/data/activity.json",
+            success: function(data) {
+                console.log(data);
+                if (data.success == 1) {
+                    var pages = data.data.pages;
+                    // maxItems = Math.ceil(pages.totalCount / perPage);
+                    if (data.data.activities) {
+                        var html = '';
+                        var ids = '';
+                        for (var i in data.data.activities) {
+                            var activity = data.data.activities[i];
 
-      // 每次加载添加多少条目
-      var itemsPerLoad = 20;
+                            if (activity == null) {
+                                continue;
+                            };
+                            ids += activity.id + ' ';
+                            html += '<div class="activityList" href="/view?id=' + activity.id + '" activityId="' + activity.id + '" class="item-link item-content">' +
+                                '<div class="activity-block" >' +
+                                '<img class="activity-photo" style="height:' + posterHeight + '" src="' + activity.poster + '" alt="activity photo"/>' +
+                                '<div class="activity-describe"><a href="/activity/view?id=' + activity.id + ' " >' + activity.content + '</a></div></div></div>';
 
-      function addItems(number, lastIndex) {
-              // 生成新条目的HTML
-              var html = '';
-              for (var i = lastIndex + 1; i <= lastIndex + number; i++) {
-                  html += '<li class="item-content"><div class="item-inner"><div class="item-title">Item ' + i + '</div></div></li>';
-              }
-              // 添加新条目
-              $('.infinite-scroll-bottom .list-container').append(html);
+                            // html  = '<img src="'+activity.poster+'"';
+                        }
+                        // console.log(html);
+                    }
 
-          }
-          //预先加载20条
-      addItems(itemsPerLoad, 0);
+                    $('.activity-list').append(html);
+                    // $('.infinite-scroll-bottom .list-container').append(html);
+                    // 如果加载数据少于4个则不显示加载框
+                    // console.log(data.data.activities.length);
+                    // if (data.data.activities.length < perPage) {
+                    //     $('.infinite-scroll-preloader').remove();
+                    // }
+                    setTimeout(function() {
+                        $('.startAnimation').hide();
+                        $('#actList').show();
+                    }, 200);
+                }
 
-      // 上次加载的序号
 
-      var lastIndex = 20;
+            },
+            error: function() {
+                console.log(11111111);
+            }
+        })
+    }
 
-      // 注册'infinite'事件处理函数
-      $(document).on('infinite', '.infinite-scroll-bottom',function() {
+getlist();
 
-          // 如果正在加载，则退出
-          if (loading) return;
 
-          // 设置flag
-          loading = true;
+    //预先加载20条
 
-          // 模拟1s的加载过程
-          setTimeout(function() {
-              // 重置加载flag
-              loading = false;
+    // 上次加载的序号
 
-              if (lastIndex >= maxItems) {
-                  // 加载完毕，则注销无限加载事件，以防不必要的加载
-                  $.detachInfiniteScroll($('.infinite-scroll'));
-                  // 删除加载提示符
-                  $('.infinite-scroll-preloader').remove();
-                  return;
-              }
+    var lastIndex = 20;
 
-              // 添加新条目
-              addItems(itemsPerLoad, lastIndex);
-              // 更新最后加载的序号
-              lastIndex = $('.list-container li').length;
-              //容器发生改变,如果是js滚动，需要刷新滚动
-              $.refreshScroller();
-          }, 1000);
-      });
-	$.init();
+    // 注册'infinite'事件处理函数
+    $(document).on('infinite', '.infinite-scroll-bottom', function() {
+
+        // 如果正在加载，则退出
+        if (loading) return;
+
+        // 设置flag
+        loading = true;
+
+        // 模拟1s的加载过程
+        setTimeout(function() {
+            // 重置加载flag
+            loading = false;
+
+            if (lastIndex >= maxItems) {
+                // 加载完毕，则注销无限加载事件，以防不必要的加载
+                $.detachInfiniteScroll($('.infinite-scroll'));
+                // 删除加载提示符
+                $('.infinite-scroll-preloader').remove();
+                return;
+            }
+
+            // 添加新条目
+            getlist();
+            // 更新最后加载的序号
+            lastIndex = $('.list-container li').length;
+            //容器发生改变,如果是js滚动，需要刷新滚动
+            $.refreshScroller();
+        }, 1000);
+    });
+    $.init();
+})
+
+Zepto(function($){
+	var getParam = function(name){
+        var search = document.location.search;
+        var pattern = new RegExp("[?&]"+name+"\=([^&]+)", "g");
+        var matcher = pattern.exec(search);
+        var items = null;
+        if(null != matcher){
+                try{
+                        items = decodeURIComponent(decodeURIComponent(matcher[1]));
+                }catch(e){
+                        try{
+                                items = decodeURIComponent(matcher[1]);
+                        }catch(e){
+                                items = matcher[1];
+                        }
+                }
+        }
+        console.log(items);
+        return items;
+};
+getParam('id');
 })
